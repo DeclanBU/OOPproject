e9ac4eb0785f3c883d58443169e54eb54af07066
Declan G. Buckley <t00189550@365s.ittralee.ie>
2016-11-17 09:24:58 +0000

    update 17-11

--- Properties.java
+++ Properties.java
@@ -9,8 +9,90 @@
 
 public class Properties {
 
-    public Properties() {
+    private float monthlyRent; 
+    private String propAddress;
+    private int noBeds;
+    private RenewableRating energyRate;    
+    
+   
+   
+    public Properties()
+    { 
+        setRent(0.0f);
+    	setPropAddress("Unknown");
+    	setNoBeds(0);
+    	setEnergyRate(0 ,"Unknown" ,"Unknown");
     }
+    
+    
+    public Properties(float rent ,String propAddress ,int noBeds ,RenewableRating energyRate)
+    {
+    	setRent(rent);
+    	setPropAddress(propAddress);
+    	setNoBeds(noBeds);
+    	setEnergyRate(energyRate);
+    }
+    
+    
+    public Properties(float rent ,String propAddress ,int noBeds ,int windowRating ,String insulation ,String heatingType)
+    {
+    	setRent(rent);
+    	setPropAddress(propAddress);
+    	setNoBeds(noBeds);
+    	setEnergyRate(windowRating ,insulation ,heatingType);
+    }
+   
+   //mutator methods
+    public void setRent(float rent)
+    {
+    	this.monthlyRent = rent;
+    }
+    public void setPropAddress(String address)
+    {
+    	this.propAddress = address;
+    }
+     public void setNoBeds(int beds)
+    {
+    	this.noBeds=beds;
+    }
+     public RenewableRating getEnergyRate()
+	 {
+	 	return energyRate;
+	 }
+    
+    
+    public void setEnergyRate(RenewableRating energyRate)
+    {
+    	this.energyRate = energyRate;
+    }
+    public void setEnergyRate(int windowRating ,String insulation ,String heatingType)
+    {
+        this.energyRate = new RenewableRating(windowRating,insulation,heatingType);	
+    }
+    
+    
+    //accessor methods
+    public float getRent()
+    {
+    	return monthlyRent;
+    }
+    public String getAddress()
+    {
+    	return propAddress;
+    }
+    public int getBeds()
+    {
+    	return noBeds;
+    }
+    
+    public String toString()
+    {
+    	return "Monthly Rent:" + getRent()+ "\nProperty Address:" + getAddress() + "No of Beds:" + getBeds() + "Energy Rating:" + getEnergyRate();
+    }	
+    
+          
+                                   
+    
     
     
 }
---
+++ RenewableRating.java
@@ -0,0 +1,54 @@
+/**
+ * @(#)RenewableRating.java
+ *
+ *
+ * @author 
+ * @version 1.00 2016/11/16
+ */
+
+
+public class RenewableRating {
+
+    private int windowRating;
+    private String insulation;
+    private String heatingType;
+    
+    public RenewableRating(int windowRating , String insulation ,String heatingType )
+    {
+    	setWindowRating(windowRating);
+    	setInsulation(insulation);
+        setHeatingType(heatingType);
+    }
+    public RenewableRating()
+    {
+    	this (0 , "unknown" ,"unknown");
+    }
+    public void setWindowRating(int winRating)
+    {
+    	this. windowRating=winRating;
+    }
+     public void setInsulation(String insulation)
+    {
+    	this. insulation=insulation;
+    }	
+    public void setHeatingType(String heatingType)
+    {
+    	this. heatingType=heatingType;
+    }	
+    public int getWindowRating()
+    {
+    	return windowRating;
+    }	
+    public String getInsulation()
+    {
+    	return insulation;
+    }
+     public String getHeatingType()
+    {
+    	return heatingType;
+    }
+    public String toString()
+    {
+    	return "Window Rating:" + getWindowRating() + "Insulation Type:" + getInsulation() + "Heating Type:" + getHeatingType();
+    }				
+}
